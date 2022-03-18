4fdc2384c2d68ac5fc4baaf894ffe8df098dc486
Pratibha <psharma@atlassian.com>
2022-03-18 20:55:43 +0000

    Exercise005 completed

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,18 @@
 package com.techreturners.exercise005;
 
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        if(input != null && !input.isEmpty()) {
+            long lettersCount = input.toLowerCase().chars().filter(c -> c >= 'a' && c <= 'z').distinct().count();
+
+            if (lettersCount == 26) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
